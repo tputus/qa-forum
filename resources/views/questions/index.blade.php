63aa36202239627cf0a0bc257251fd12cdb8c253
tputus <teddyputus@hotmail.co.uk>
2020-04-02 23:35:14 +0100

    add author infor and question creation date

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="title m-b-md">Q&A Forum</div>
+        <div class="card">
+          <div class="card-header">All Questions</div>
+
+          <div class="card-body">
+            @foreach($questions as $question)
+              <div class="media">
+                <div class="media-body">
+                  <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                  <p class="lead">
+                    Asked by
+                    <a href="{{$question->url}}">{{ $question->user->name}}</a>
+                    <small class="text-muted">{{ $question->created_date }}</small>
+                  </p>
+                  {{ Str::limit($question->body, 250) }}
+                </div>
+              </div>
+              <hr>
+            @endforeach
+
+            {{ $questions->links() }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
