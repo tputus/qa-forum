4eb10c1b7d55990fe2d27c1f58f86d640705d74f
tputus <teddyputus@hotmail.co.uk>
2020-04-02 14:43:39 +0100

    Prepopulate user and question tqables with factories and fixes

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -41,9 +42,6 @@ class User extends Authenticatable
       return $this->hasMany(Question::class);
     }
 
-    public function setTitleAttribute($value){
-      $this->attributes['title'] = $value;
-      $this->attributes['slug'] = Str::slug($value);
-    }
+  
 
 }
